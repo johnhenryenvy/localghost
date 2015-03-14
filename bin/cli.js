25ab71ace40935e3850bc27034ad14fa0c002243
John <john@iamjohnhenry.com>
2015-03-14 09:31:13 -0700

    Remove commands that will not be used

--- bin/cli.js
+++ bin/cli.js
@@ -28,39 +28,10 @@ program
   .option("-v --verbose", "show debug output", increaseVerbosity)
 
 program
-  .command("init [project_id]")
-  .description("Initialize an optimizely project.")
-  .option("-r --remote", "initialize from remote project")
-  .option("-j --jquery", "include jquery (local project only)")
-  .action(loadCommand("init-project"));
-
-program
-  .command("experiment <folder> <description> <url>")
-  .description("Create Local Experiment")
-  .action(loadCommand("create-experiment"));
-
-program
-  .command("variation <experiment> <folder> <description>")
-  .description("Create Local Variation")
-  .action(loadCommand("create-variation"));
-
-program
   .command("host <path> [port]")
   .option("-s --ssl", "SSL")
   .description("Host variation locally")
   .action(loadCommand("host"));
-
-program
-  .command("push-experiment <path>")
-  .option("-i --iterate", "Push experiment iterate through all variations")
-  .description("Push an experiment to Optimizely")
-  .action(loadCommand("push-experiment"));
-
-program
-  .command("push-variation <path>")
-  .description(
-    "Push a variation to Optimizely (experiment must be pushed first)")
-  .action(loadCommand("push-variation"));
 
 //Show help if no arguments are passed
 if (!process.argv.slice(2).length) {
